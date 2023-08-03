f4131d572fa2a1c68eacf037ce88dd99c8580160
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2023-08-03 18:21:58 -0500

    Created list entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NoteModule } from './note/note.module';
+import { ListModule } from './list/list.module';
 
 @Module({
-  imports: [NoteModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'todoDB',
+      entities: [],
+      synchronize: true,
+    }),
+    NoteModule,
+    ListModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
