24053c853f922b9b377a8cc0be117ce10bb3cb40
freddychannarvaez <freddy.chan.narvaez@gmail.com>
2023-08-16 14:45:53 -0600

    Changed id from bigint to int

--- src/list/entities/list.entity.ts
+++ src/list/entities/list.entity.ts
@@ -4,7 +4,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class List {
   @PrimaryGeneratedColumn({
-    type: 'bigint',
+    type: 'int',
   })
   id: number;
 
--- src/note/entities/note.entity.ts
+++ src/note/entities/note.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Note {
   @PrimaryGeneratedColumn({
-    type: 'bigint',
+    type: 'int',
   })
   id: number;
 
